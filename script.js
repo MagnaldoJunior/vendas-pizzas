6119a3efc57dd55c2e06db88edaa50596906e3c5
MagnaldoJunior <magnaldojunior01@gmail.com>
2021-08-14 01:15:42 -0300

    informacoes das pizzas

--- script.js
+++ script.js
@@ -5,5 +5,8 @@ const cs = (el)=>document.querySelectorAll(el);
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
     c('.pizza-area').append( pizzaItem );  
 });
